59857d6cfa0b4e4a97ac6d702cb6fa0bbb95f86b
Russell Owen <rowen@uw.edu>
2013-03-14 15:32:37 -0700

    Add missing files to git repo. SWIG mathUtils.h

---
+++ src/mountCoordSys.cc
@@ -0,0 +1,32 @@
+#include <stdexcept>
+#include <sstream>
+#include "coordConv/mathUtils.h"
+#include "coordConv/coordSys.h"
+
+namespace coordConv {
+
+    MountCoordSys::MountCoordSys(double date)
+    :
+        ApparentCoordSys("mount", date)
+    {
+        setDate(date);
+    };
+    
+    CoordSys::Ptr MountCoordSys::clone() const {
+        return clone(getDate());
+    }
+
+    CoordSys::Ptr MountCoordSys::clone(double date) const {
+        return CoordSys::Ptr(new MountCoordSys(date));
+    };
+
+    Coord MountCoordSys::fromFK5J2000(Coord const &coord, Site const &site) const {
+        return Coord(); // null Coord
+    }
+
+    Coord MountCoordSys::toFK5J2000(Coord const &coord, Site const &site) const {
+        return Coord(); // null Coord
+    }
+
+}
+
---
+++ src/noneCoordSys.cc
@@ -0,0 +1,32 @@
+#include <stdexcept>
+#include <sstream>
+#include "coordConv/mathUtils.h"
+#include "coordConv/coordSys.h"
+
+namespace coordConv {
+
+    NoneCoordSys::NoneCoordSys(double date)
+    :
+        ApparentCoordSys("none", date)
+    {
+        setDate(date);
+    };
+    
+    CoordSys::Ptr NoneCoordSys::clone() const {
+        return clone(getDate());
+    }
+
+    CoordSys::Ptr NoneCoordSys::clone(double date) const {
+        return CoordSys::Ptr(new NoneCoordSys(date));
+    };
+
+    Coord NoneCoordSys::fromFK5J2000(Coord const &coord, Site const &site) const {
+        return Coord(); // null Coord
+    }
+
+    Coord NoneCoordSys::toFK5J2000(Coord const &coord, Site const &site) const {
+        return Coord(); // null Coord
+    }
+
+}
+
