5822aaa06ee430f5ad11ca3605da2f4e437a10e7
Russell Owen <rowen@uw.edu>
2013-05-08 17:18:20 -0700

    Tweak up testWrap: correct the name, reduce redundant computation of sin(ang) and cos(ang).

--- tests/testWrap.py
+++ tests/testWrap.py
@@ -6,20 +6,23 @@ from coordConv import wrapCtr, wrapPos, wrapNear, sind, cosd, PVT
 Eps = 2e-14
 
 class TestWrap(unittest.TestCase):
-    def testWrapPos(self):
+    def testWrap(self):
         for wrap in (-1000, -10, -1, 0, 1, 10, 1000):
             for offset in (-360, -180, -90, 0, 90, 180, 270, 360):
                 for epsMult in (-10, -4, -2, -1, 0, 1, 2, 4, 10):
                     ang = (wrap * 360) + offset + (epsMult * Eps)
+                    sinAng = sind(ang)
+                    cosAng = cosd(ang)
                     pvt = PVT(ang, ang, 35.0) # pick anything for vel and time
 
                     posAng = wrapPos(ang)
                     self.assertGreaterEqual(posAng, 0.0)
                     self.assertLess(posAng, 360.0)
                     # prove that posAng and ang are the same angle
+                    # sin and cos are a sanity check on wrapCtr
                     self.assertAlmostEqual(wrapCtr(posAng - ang), 0)
-                    self.assertAlmostEqual(sind(posAng), sind(ang))
-                    self.assertAlmostEqual(cosd(posAng), cosd(ang))
+                    self.assertAlmostEqual(sind(posAng), sinAng)
+                    self.assertAlmostEqual(cosd(posAng), cosAng)
 
                     posPvt = wrapPos(pvt)
                     self.assertEqual(posPvt.pos, posAng)
@@ -31,24 +34,24 @@ class TestWrap(unittest.TestCase):
                     self.assertLess(ctrAng, 180.0)
                     # prove that ctrAng and ang are the same angle
                     self.assertAlmostEqual(wrapCtr(ctrAng - ang), 0)
-                    self.assertAlmostEqual(sind(ctrAng), sind(ang))
-                    self.assertAlmostEqual(cosd(ctrAng), cosd(ang))
+                    self.assertAlmostEqual(sind(ctrAng), sinAng)
+                    self.assertAlmostEqual(cosd(ctrAng), cosAng)
 
                     ctrPvt = wrapCtr(pvt)
                     self.assertEqual(ctrPvt.pos, ctrAng)
                     self.assertEqual(ctrPvt.vel, pvt.vel)
                     self.assertEqual(ctrPvt.t, pvt.t)
                     
-                    for otherAngBase in (-180, 0, 180, 360):
-                        for otherEpsMult in (-10, -4, -2, -1, 0, 1, 2, 4, 10):
-                            otherAng = otherAngBase + (otherEpsMult * Eps)
-                            nearAng = wrapNear(ang, otherAng)
-                            self.assertGreaterEqual(nearAng - otherAng, -180)
-                            self.assertLess(nearAng - otherAng, 180)
+                    for refAngBase in (-180, 0, 180, 360):
+                        for refEpsMult in (-10, -4, -2, -1, 0, 1, 2, 4, 10):
+                            refAng = refAngBase + (refEpsMult * Eps)
+                            nearAng = wrapNear(ang, refAng)
+                            self.assertGreaterEqual(nearAng - refAng, -180)
+                            self.assertLess(nearAng - refAng, 180)
                             # prove that nearAng and ang are the same angle
                             self.assertAlmostEqual(wrapCtr(nearAng - ang), 0)
-                            self.assertAlmostEqual(sind(nearAng), sind(ang))
-                            self.assertAlmostEqual(cosd(nearAng), cosd(ang))
+                            self.assertAlmostEqual(sind(nearAng), sinAng)
+                            self.assertAlmostEqual(cosd(nearAng), cosAng)
                             
 
 if __name__ == '__main__':
