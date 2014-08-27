5244bca24407180fb1b970c77f398a91b4dcfc5a
Russell Owen <rowen@uw.edu>
2014-08-27 11:02:37 -0700

    Updated timeCoordConv to test PVT conversions

--- examples/timeCoordConv.py
+++ examples/timeCoordConv.py
@@ -54,9 +54,19 @@ def timeFK5ToAppTopo(alt, niter):
     startTime = time.time()
     for i in range(niter):
         tai = currTAI + (i * 0.1)
-        appTopoCoord, dir, scaleChange = appTopoSys.convertFrom(fk5Sys, fk5Coord, 5.0, site, tai)
+        appTopoCoord, toDir, scaleChange = appTopoSys.convertFrom(fk5Sys, fk5Coord, 5.0, site, tai)
     duration = time.time() - startTime
-    print "FK5 to AppTopo: %0.1f conversions/second (%d conversions in %0.2f sec) at alt=%0.1f" % (niter/duration, niter, duration, alt)
+    print "FK5 to AppTopo    Coord with dir: %8.1f conversions/second (%d conversions in %0.2f sec) at alt=%0.1f" % (niter/duration, niter, duration, alt)
+
+    startTime = time.time()
+    fk5PVTCoord = coordConv.PVTCoord(fk5Coord, fk5Coord, currTAI, 0.01)
+    toPVTDir = coordConv.PVT()
+    fromPVTDir = coordConv.PVT(5, 0, tai)
+    for i in range(niter):
+        tai = currTAI + (i * 0.1)
+        appTopoPVTCoord, scaleChange = appTopoSys.convertFrom(toPVTDir, fk5Sys, fk5PVTCoord, fromPVTDir, site)
+    duration = time.time() - startTime
+    print "FK5 to AppTopo PVTCoord with dir: %8.1f conversions/second (%d conversions in %0.2f sec) at alt=%0.1f" % (niter/duration, niter, duration, alt)
     
 def timeAppTopoToFK5(alt, niter):
     """Time apparent topocentric to FK5 at the specified altitude
@@ -77,11 +87,22 @@ def timeAppTopoToFK5(alt, niter):
     appTopoCoord = coordConv.Coord(120, alt)
     site = makeSite()
     startTime = time.time()
+    fromDir = 5.0
     for i in range(niter):
         tai = currTAI + (i * 0.1)
-        fk5Coord, dir, scaleChange = fk5Sys.convertFrom(appTopoSys, appTopoCoord, 5.0, site, tai)
+        fk5Coord, toDir, scaleChange = fk5Sys.convertFrom(appTopoSys, appTopoCoord, fromDir, site, tai)
     duration = time.time() - startTime
-    print "AppTopo To FK5: %0.1f conversions/second (%d conversions in %0.2f sec) at alt=%0.1f" % (niter/duration, niter, duration, alt)
+    print "AppTopo To FK5    Coord with dir: %0.1f conversions/second (%d conversions in %0.2f sec) at alt=%0.1f" % (niter/duration, niter, duration, alt)
+
+    startTime = time.time()
+    appTopoPVTCoord = coordConv.PVTCoord(appTopoCoord, appTopoCoord, currTAI, 0.01)
+    toPVTDir = coordConv.PVT()
+    fromPVTDir = coordConv.PVT(5, 0, tai)
+    for i in range(niter):
+        tai = currTAI + (i * 0.1)
+        fk5PVTCoord, scaleChange = fk5Sys.convertFrom(toPVTDir, appTopoSys, appTopoPVTCoord, fromPVTDir, site)
+    duration = time.time() - startTime
+    print "AppTopo To FK5 PVTCoord with dir: %0.1f conversions/second (%d conversions in %0.2f sec) at alt=%0.1f" % (niter/duration, niter, duration, alt)
 
 def timeAppGeoData(niter):
     """Time computation of apparent geocentric data
